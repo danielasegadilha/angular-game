12e3090274407c5c5e880680f15cbedc3a831a73
Daniela <danisegd@hotmail.com>
2024-02-28 09:42:01 -0300

    atualização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharadeComponent } from './components/charade/charade.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ButtonComponent } from './components/button/button.component';
+import { BrandComponent } from './components/brand/brand.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharadeComponent,
+    HomeComponent,
+    ButtonComponent,
+    BrandComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/charade/charade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-charade',
+  templateUrl: './charade.component.html',
+  styleUrls: ['./charade.component.css']
+})
+export class CharadeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
