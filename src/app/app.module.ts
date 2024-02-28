65662e67b1858a2c0a02ae42b9662e9d8870c2aa
Daniela <danisegd@hotmail.com>
2024-02-28 20:37:17 -0300

    Add question component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,10 @@ import { AppComponent } from './app.component';
 import { HomeModule } from './components/home/home.module';
 import { InstructionModule } from './components/instruction/instruction.module';
 import { GameModule } from './components/game/game.module';
-import { RuleComponent } from './instruction/how-to-play/rule/rule.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RuleComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/game/game.module.ts
+++ src/app/components/game/game.module.ts
@@ -4,9 +4,10 @@ import { ResultComponent } from './result/result.component';
 import { CharadeComponent } from './charade/charade.component';
 import { GameRoutingModule } from './game-routing.module';
 import { FeedbackComponent } from './result/feedback/feedback.component';
+import { QuestionComponent } from './charade/question/question.component';
 
 @NgModule({
-  declarations: [CharadeComponent, ResultComponent, FeedbackComponent],
+  declarations: [CharadeComponent, ResultComponent, FeedbackComponent, QuestionComponent],
   imports: [SharedModule, GameRoutingModule]
 })
 
