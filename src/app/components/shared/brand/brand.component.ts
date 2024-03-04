12405fc5e964c406a61b1ca4f36773066c64a481
Daniela <danisegd@hotmail.com>
2024-03-04 16:11:50 -0300

    Create data interpolation for brand component

--- src/app/components/shared/brand/brand.component.ts
+++ src/app/components/shared/brand/brand.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-brand',
@@ -12,4 +12,5 @@ export class BrandComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  @Input() containerHeight: String = '';
 }
