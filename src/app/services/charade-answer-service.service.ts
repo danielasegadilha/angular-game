96e70f7f792280ab3a2d04e3634891862b281f01
Daniela <danisegd@hotmail.com>
2024-03-06 17:40:15 -0300

    Add observable

--- src/app/services/charade-answer-service.service.ts
+++ src/app/services/charade-answer-service.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharadeAnswerService {
-  private apiUrl = ''
+  private apiUrl: String = ''
   
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.charadeAPI
+   }
+
+   getCharade(charadeQuestion: String):Observable<String> {
+    this.http.get(`${this.apiUrl}`)
+    console.log(charadeQuestion)
+   }
+
 }
--- src/app/services/charade-service.service.ts
+++ src/app/services/charade-service.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharadeService {
-  private apiUrl = ''
+  private apiUrl: String = ''
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    this.apiUrl = environment.charadeAPI
+  }
 }
